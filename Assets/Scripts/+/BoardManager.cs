70673b7369c0ed36e4576ef142e0a3fba5c0899b
Incognit00 <73686096+Incognit00@users.noreply.github.com>
2021-05-22 07:54:34 +0300

    Update BoardManager.cs

--- Assets/Scripts/+/BoardManager.cs
+++ Assets/Scripts/+/BoardManager.cs
@@ -80,7 +80,7 @@ public class BoardManager : MonoBehaviour {
 		}
 
 		for (int i = 0; i < nullCount; i++) {
-			//GUIManager.instance.Score += 50; // Add this line here
+			GUIManager.instance.Score += 10; // Add this line here
 			yield return new WaitForSeconds(shiftDelay);
 			for (int k = 0; k < renders.Count - 1; k++) {
 				renders[k].sprite = renders[k + 1].sprite;
